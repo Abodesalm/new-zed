8ab55ef9e08bf44e7a785f1cafb431e77ce6a9b2
abod <abodesalm45@gmail.com>
2025-03-03 06:47:24 +0300

    first commit

--- app/[lang]/not-found.tsx
+++ app/[lang]/not-found.tsx
@@ -1,10 +1,7 @@
-import { useTranslations } from "next-intl";
-
 const Not = () => {
-  const t = useTranslations("not_found");
   return (
     <div className="capitalize h-[70vh] flex items-center justify-center font-black text-[24px]">
-      {t("not_found")}
+      not found
     </div>
   );
 };
